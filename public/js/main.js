e718920480a87c07075f8f565b3cac5f084b1b2f
jin-pp23 <pp2369@naver.com>
2018-12-18 14:28:48 +0900

    2018.12.18

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ function winOpen(){
     window.open("https://pp23-gestbook.firebaseapp.com","win","width:400,height:400,top:100,left:100")
 }
 
-var now = 1 ;
-function ani(){$(".slide").delay(2000).animate({"left"})}
+// var now = 1 ;
+// function ani(){$(".slide").delay(2000).animate({"left"})}
