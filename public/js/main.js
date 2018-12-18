97168f99b2f3db3dfa80d7acf876313f6298b477
booldook <booldook@gmail.com>
2018-12-18 15:34:17 +0900

    booldook

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,20 @@ function winOpen(){
 
 // var now = 1 ;
 // function ani(){$(".slide").delay(2000).animate({"left"})}
+
+$(".slides").append($(".slide").eq(0).clone());
+$(".slide").each(function(i){
+    $(this).css({"left":(i*100)+"%"});
+});
+
+var num = 1;
+var interval = setInterval(slideFn, 3000);
+function slideFn() {
+    $(".slides").stop().animate({"left":-(num*100)+"%"}, 500, function(){
+        if(num == $(".slide").length - 1) {
+            $(this).css({"left":0});
+            num = 0;
+        }
+        num++;
+    });
+}
